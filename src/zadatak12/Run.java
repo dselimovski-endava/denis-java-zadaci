c3dd25c1c8e33a4fcdaccf9011d6dd2de1b70493
Denis Selimovski <Denis.Selimovski@endava.com>
2018-09-14 11:58:49 +0200

    dodajem zadatak12

---
+++ src/zadatak12/Run.java
@@ -0,0 +1,83 @@
+package zadatak12;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Collections;
+
+/**
+ * @author Denis.Selimovski
+ *
+ */
+
+/**
+* Napisati program koji umogucava unos imena studenta i broj poena osvojenih na testu preko
+* standardnog ulaza (sve dok se ne unese odredjen znak za prekid unosa podataka).
+* Broj poena sme da bude od 0 do 100.
+* Kao rezultat programa,  ocekuje se izlazni fiajl "ocene.txt" u kome ce se nalaziti lista 
+* svih studenata sortirana po poenima, od najvise ka najmanje, i gde ce se osim imena studenta
+*  i boja poena nalaziti i ocena koju je dobio na ispiti.
+*  Ocena 10 akoje imao vise od 90 poena, 9 ako je imao izmedju 81 i 90... Sve do 5 ako je imao 
+*  manje od 51 poena. 
+ */
+public class Run {
+
+	public static Scanner ulaz = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		
+		ArrayList<Student> listaStudenata = new ArrayList<>();
+		boolean nastavi = true;
+		
+		do {
+			System.out.print("Unesite ime studenta: ");
+			String ime = unosImena();
+			
+			System.out.print("Unesite broj poena studenta: ");
+			double brojPoena = unosPoena();
+			
+			Student student = new Student(ime, brojPoena);
+			listaStudenata.add(student);
+			
+			System.out.print("Ukoliko zelite da prekinete dalji unos ukucajte 'ne': ");
+			String nastaviStr = ulaz.next();
+			if(nastaviStr.equalsIgnoreCase("ne"))
+				nastavi = false;
+		} while(nastavi);
+		
+		Collections.sort(listaStudenata);
+		
+		File fajl = new File("ocene.txt");
+		try {
+			PrintWriter pw = new PrintWriter(fajl);
+			for (Student student : listaStudenata) {
+				pw.println(student);
+			}
+			pw.close();
+			System.out.println("Podaci uspesno upisani u fajl...");
+		} catch (IOException ex) {
+			System.out.println(ex);
+		} finally {
+			System.out.println("--- KRAJ PROGRAMA ---");
+		}
+		ulaz.close();
+	}
+	
+	public static String unosImena() {
+		while (!ulaz.hasNext("[a-zA-Z]+")) {
+			System.out.print("Pogresan unos, unesite ime studenta: ");
+			ulaz.next();
+		} 
+		return ulaz.next();
+	}
+	
+	public static double unosPoena() {
+		while (!ulaz.hasNextDouble()) {
+			System.out.print("Pogresan unos, unesite broj poena: ");
+			ulaz.next();
+		} 
+		return ulaz.nextDouble();
+	}
+}
---
+++ src/zadatak12/Student.java
@@ -0,0 +1,75 @@
+package zadatak12;
+
+/**
+ * @author Denis.Selimovski
+ *
+ */
+public class Student implements Comparable<Student> {
+	
+	private String ime;
+	private double brojPoena;
+	
+	public Student() {
+	}
+	
+	public Student(String ime, double brojPoena) {
+		this.ime = ime;
+		if(0 <= brojPoena && 100 >= brojPoena)
+			this.brojPoena = brojPoena;
+		else if(0 > brojPoena)
+			this.brojPoena = 0;
+		else
+			this.brojPoena = 100;
+	}
+
+	public String getIme() {
+		return ime;
+	}
+	
+	public void setIme(String ime) {
+		this.ime = ime;	
+	}
+	
+	public double getBrojPoena() {
+		return brojPoena;
+	}
+	
+	public void setBrojPoena(double brojPoena) {
+		if(0 <= brojPoena && 100 >= brojPoena)
+			this.brojPoena = brojPoena;
+		else if(0 > brojPoena)
+			this.brojPoena = 0;
+		else
+			this.brojPoena = 100;
+	}
+
+	public int odrediOcenu() {
+		if(91 <= brojPoena)
+			return 10;
+		else if(81 <= brojPoena && 90 >= brojPoena)
+			return 9;
+		else if(71 <= brojPoena && 80 >= brojPoena)
+			return 8;
+		else if(61 <= brojPoena && 70 >= brojPoena)
+			return 7;
+		else if(51 <= brojPoena && 60 >= brojPoena)
+			return 6;
+		else
+			return 5;
+	}
+	
+	@Override
+	public int compareTo(Student s) {
+		if(this.getBrojPoena() == s.getBrojPoena())
+			return 0;
+		else if(this.getBrojPoena() > s.getBrojPoena())
+			return -1;
+		else
+			return 1;
+	}
+	
+	@Override
+	public String toString() {
+		return ime + " - broj poena: " + brojPoena + ", ocena: " + odrediOcenu();
+	}
+}
