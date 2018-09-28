f5104b64354ac0f51ac1bebedd559d4530e5ed7d
Denis Selimovski <Denis.Selimovski@endava.com>
2018-09-28 13:10:02 +0200

    zatvoren scanner

--- src/zadatak13/Run.java
+++ src/zadatak13/Run.java
@@ -12,33 +12,35 @@ import java.util.Scanner;
  */
 
 /**
-* Napisati isti program kao u Zadatku 2 koji vraca drugaciji rezultat: imena studenta i poeni na testu 
-* se nalaze u nekom fajlu "poeni.txt". Zadatak je ispisati na standardni izlaz spisak studenata (ime, 
-* poeni i ocena) sortiran po poenima.
+ * Napisati isti program kao u Zadatku 2 koji vraca drugaciji rezultat: imena
+ * studenta i poeni na testu se nalaze u nekom fajlu "poeni.txt". Zadatak je
+ * ispisati na standardni izlaz spisak studenata (ime, poeni i ocena) sortiran
+ * po poenima.
  */
 public class Run {
 
 	public static Scanner sc;
-	
+
 	public static void main(String[] args) {
-		
+
 		ArrayList<Student> listaStudenata = new ArrayList<>();
 		File fajl = new File("poeni.txt");
-		
-			try {
-				sc = new Scanner(fajl);
-				while(sc.hasNext()) {
-					String ime = sc.next();
-					double poeni = sc.nextDouble();
-					Student student = new Student(ime, poeni);
-					listaStudenata.add(student);
-				}
-				Collections.sort(listaStudenata);
-				for (Student s : listaStudenata) {
-					System.out.println(s.toString());
-				}
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
+
+		try {
+			sc = new Scanner(fajl);
+			while (sc.hasNext()) {
+				String ime = sc.next();
+				double poeni = sc.nextDouble();
+				Student student = new Student(ime, poeni);
+				listaStudenata.add(student);
 			}
+			sc.close();
+			Collections.sort(listaStudenata);
+			for (Student s : listaStudenata) {
+				System.out.println(s.toString());
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 }
